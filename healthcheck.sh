72971afa994f63c292d4a00e6e871691b03fb58c
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-08-07 11:33:03 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,5 +5,10 @@ if [ "$(netstat -plnt | grep -c 4040)" -ne 1 ]; then
    exit 1
 fi
 
+if [ "$(ip -o addr | grep "$(hostname -i)" | wc -l)" -eq 0 ]; then
+   echo "NIC missing"
+   exit 1
+fi
+
 echo "Airsonic ports 4040 responding OK"
 exit 0
